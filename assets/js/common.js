01351dd5a3ff452373a1ab96ee8396e95ba94639
guaran <2833198046@qq.com>
2022-01-19 08:18:53 +0800

    index页面完成

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,1 +1,29 @@
 axios.defaults.baseURL = 'http://www.liulongbin.top:3007'
+
+// 请求拦截器 设置免登陆
+axios.interceptors.request.use(function (config) {
+  // console.log(config)
+  if (config.url.indexOf('/my/') != -1) {
+    config.headers.Authorization = localStorage.getItem('token')
+  }
+
+  return config
+}, function (error) {
+
+  return Promise.reject(error)
+})
+
+// 响应拦截器 登录拦截
+axios.interceptors.response.use(function (response) {
+  console.log(response)
+  if (response.data.message == '身份认证失败！') {
+    // 跳回登录页
+    location.href = '/login.html'
+    // 销毁token
+    localStorage.removeItem('token')
+  }
+  return response
+}, function (error) {
+
+  return Promise.reject(error)
+})
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,35 @@
 jQuery(function () {
 
+  // 1. 发送ajax渲染头像
   getUserInfo()
+
+  // 2. 退出登录
+  $('#loginout').on('click', function () {
+    // 弹出层询问
+    layer.confirm('确认退出登录吗？', {
+      icon: 3,
+      title: '提示'
+    }, function (index) {
+      //do something
+      // 跳回登录页
+      location.href = '/login.html'
+      // 销毁token
+      localStorage.removeItem('token')
+
+      layer.close(index);
+    });
+  })
+
 })
 
 
 function getUserInfo() {
   axios({
     url: '/my/userinfo',
-    method: 'GET',
-    headers: {
-      Authorization: localStorage.getItem('token')
-    }
+    // method: 'GET',
+    // headers: {
+    //   Authorization: localStorage.getItem('token')
+    // }
   }).then(({
     data: res
   }) => {
@@ -18,10 +37,24 @@ function getUserInfo() {
     if (res.status !== 0) {
       return layer.msg(res.message)
     }
-
+    renderAvatar(res.data)
   });
 }
 
-function renderAvatar() {
+function renderAvatar(user) {
+  // 1. 渲染欢迎词 优先渲染昵称 没有再渲染用户名渲染
+  let name = user.nickname || user.username
+  $('.welcome').html('欢迎你！ ' + name)
 
+  // 2. 渲染头像 判断用户是否有头像
+  if (user.user_pic !== null) {
+    // 2.1 如果有 就显示图片头像  隐藏文字头像 
+    $('.layui-nav-img').show().prop('src', user.user_pic)
+    $('.avatar-text').hide()
+  } else {
+    // 2.2 如果没有 显示文字头像 隐藏图片头像 
+    $('.layui-nav-img').hide()
+    // 把用户名的首字母改为大写 作为头像渲染到页面上
+    $('.avatar-text').show().html(name[0].toUpperCase())
+  }
 }
